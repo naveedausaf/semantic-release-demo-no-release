ef1f34e7afd423389b54cf7afb3c7ce70185fcd9
naveedausaf <20386346+naveedausaf@users.noreply.github.com>
2024-10-04 17:32:11 +0100

    ci: changes

--- cli.js
+++ cli.js
@@ -3,5 +3,5 @@ import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 const packageJson = require("./package.json");
 
-console.log("Hello World!");
+console.log("Hello there!");
 console.log(`Version number of the package is ${packageJson.version}`);
